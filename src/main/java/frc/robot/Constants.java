d7b741a9227e747502a53dad0095cc4c46e3bda8
Luke Rossi <lukerpro5913@gmail.com>
2025-04-14 14:35:51 -0400

    started structuring the code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,16 +4,110 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.util.Units;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
+
+  public static String armPreset = "L2"; //L1, L2, L3, L4, coralstation, 
+
+  public static class DriverConstants {
+    public static final int MAIN_DRIVER_PORT = 0;
+    public static final int AUX_DRIVER_PORT = 1;
+    public static final int DRIVER_STATION_PORT = 2;
+    // public static final String DEADBAND = "0.5";
+    public static final double DEADBAND = 0;
+    public static final double MAX_SPEED = Units.feetToMeters(9); //4.5
+    public static final int[] driverstationButtons = { 14, 20, 30, 40 }; // 0 = top left, and then read it like a book
   }
+
+  public static class CandleConstants {
+    public static final int CANDLE_ID = 57;
+  }
+
+  public static class WristConstants {
+    public static final double WRIST_MOTOR_SPEED = 0.2;
+    public static final double WRIST_POSITION_TWO = 5;
+    public static final double WRIST_POSITION_ONE = 0;
+  }
+
+  public static class ShoulderConstants {
+    public static final int SHOULDER_MOTOR_ID = 50;
+    public static final int ABSOLUTE_ENCODER_CHANNEL = 1;
+
+    public static final double SHOULDER_MOTOR_SPEED = 0.2;
+
+    public static final double TOTAL_OFFSET = 4;
+
+    public static final double L1_SETPOINT = -70 + TOTAL_OFFSET;
+    public static final double L2_SETPOINT = -30 + TOTAL_OFFSET;
+    public static final double L3_SETPOINT = 8 + TOTAL_OFFSET;
+    public static final double L4_SETPOINT = 48 + TOTAL_OFFSET;
+
+    public static final double CORAL_STATION_SETPOINT = 6; //4
+    public static final double GROUND_SETPOINT = -70; // TODO
+    public static final double GROUND_VERTICAL_SETPOINT = -170;
+    public static final double UPPER_ALGAE_SETPOINT = 3.927; // TODO
+    public static final double LOWER_ALGAE_SETPOINT = -67.7; // TODO
+  }
+
+  public static class IntakeConstants {
+    public static final double INTAKE_MOTOR_SPEED = 1;
+    public static final double INTAKE_OUT_MOTOR_SPEED = 0.7;
+    public static final double INTAKE_OUT_MOTOR_SLOW_SPEED = 0.3;
+    public static final double DISTANCE_FROM_PIECE = 1; // This value is in inches
+    public static final double INTAKE_PULSE_INTERVAL = 2.5; // This value is in seconds
+    public static final double INTAKE_PULSE_LENGTH = 0; // This value is in seconds
+  }
+
+  public static class ActuatorConstants {
+    public static final int length = 100; // Servo Length
+  }
+
+  public static class ElevatorConstants {
+    public static final int ELEVATOR_MOTOR_ID = 51;
+    public static final int ELEVATOR_LIMITSWITCH_CHANNEL = 0;
+
+    public static final double ELEVATOR_MOTOR_SPEED = 0.2;
+    public static final double ELEVATOR_LIMIT = 230;
+
+    public static final double TOTAL_OFFSET = 0;
+
+    public static final double SAFE_RESET_SETPOINT = 5;
+    public static final double L1_SETPOINT = 0 + TOTAL_OFFSET;
+    public static final double L2_SETPOINT = 40 + TOTAL_OFFSET;
+    public static final double L3_SETPOINT = 108.275 + TOTAL_OFFSET;
+    public static final double L4_SETPOINT = 230 + TOTAL_OFFSET;
+    public static final double CORAL_STATION_SETPOINT = 35 + TOTAL_OFFSET; //32
+    public static final double GROUND_SETPOINT = 20.115; // TODO
+    public static final double GROUND_VERTICAL_SETPOINT = 30;
+    public static final double UPPER_ALGAE_SETPOINT = 90.941 + TOTAL_OFFSET; // TODO
+    public static final double LOWER_ALGAE_SETPOINT = 22 + TOTAL_OFFSET; // TODO
+  }
+
+  public static final class LimelightConstants {
+    /** upward angle of limelight camera [degrees] */
+    public static final double LIMELIGHT_ANGLE = 3.0;
+    /** distance from limelight lens from floor [inches] */
+    public static final double LIMELIGHT_HEIGHT = 18.5;
+    /** distance from apriltag to floor(bottom of tag) [inches] */
+    public static final double APRILTAG_HEIGHT = 14.25;
+    /**
+     * distance from apriltag to floor but its the double substation(bottom of tag)
+     * [inches]
+     */
+    public static final double APRILTAG_DOUBLE_SUBSTATION_HEIGHT = 23.375;
+  }
+
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,28 +5,25 @@
 package frc.robot;
 
 import frc.lib.LimelightCam;
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.Constants.DriverConstants;
+import frc.robot.subsystems.SwerveSubsystem;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
+ * The robot container for iced java, this is where we declare subsystems, and bind our commands to our controllers.
+ * We also can declare autonomous here officially.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  // Subsystem declaration
+  private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  // Controller declaration
+  private final CommandXboxController driverController = new CommandXboxController(DriverConstants.MAIN_DRIVER_PORT);
+  private final CommandXboxController operatorController = new CommandXboxController(DriverConstants.MAIN_DRIVER_PORT);
+  private final CommandXboxController driverStation = new CommandXboxController(DriverConstants.MAIN_DRIVER_PORT);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -45,13 +42,7 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
   /**
@@ -61,6 +52,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    // return Autos.exampleAuto(m_exampleSubsystem);
   }
 }
