eb7fe744b81366d8023b38004adf392167472772
Mochatitan <mochatitanstudios@gmail.com>
2025-04-21 19:09:01 -0400

    Added some concept files

---
+++ src/main/java/frc/lib/controller/ControllerUtil.java
@@ -0,0 +1,5 @@
+package frc.lib.controller;
+
+public class ControllerUtil {
+    //Static method for turning axis into a range of whatever yoyu want it to be   
+}
---
+++ src/main/java/frc/lib/controller/MultiCommandButton.java
@@ -0,0 +1,5 @@
+package frc.lib.controller;
+
+public class MultiCommandButton {
+        // A joystick button that can do a different command based on what number or boolean you give it.
+}
